8a1e107b647cdeb062b573fc532e55c968cb3fa4
Shreedhar Hegde <shreedharhegade96@gmail.com>
2019-12-07 11:58:22 +0530

    added active tab style

--- client/src/App.js
+++ client/src/App.js
@@ -1,85 +1,115 @@
 import React from "react";
 import TimeTableForm from "./components/TimeTableForm";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 import Header from "./components/Header";
 import FormsNavbar from "./components/add-data/Navbar";
+import PropTypes from "prop-types";
 
-export default function App() {
-  return (
-    <div className="container-fluid">
-      <div className="row">
-        <div className="col-md-12">
-          <Header />
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-12">
+            <Header />
+          </div>
         </div>
+
+        <Router>
+          <div style={{ margin: 0 }}>
+            <nav>
+              <ul className="navUl">
+                <li className="navLi">
+                  <h3>
+                    <NavLink
+                    exact
+                      to="/"
+                      activeStyle={{
+                        "backgroundColor":" #555",
+                        "color": "white",
+                        "textDecoration": "none"
+                      }}
+                    >
+                      Home
+                    </NavLink>
+                  </h3>
+                </li>
+                <li className="navLi">
+                  <h3>
+                    <NavLink
+                       activeStyle={{
+                        "backgroundColor":" #555",
+                        "color": "white",
+                        "textDecoration": "none"
+                      }}
+                      to="/add-data"
+                    >
+                      Add data
+                    </NavLink>
+                  </h3>
+                </li>
+                <li className="navLi">
+                  <h3>
+                    <NavLink
+                       activeStyle={{
+                        "backgroundColor":" #555",
+                        "color": "white",
+                        "textDecoration": "none"
+                      }}
+                      to="/createtimetable"
+                    >
+                      Create Time Table
+                    </NavLink>
+                  </h3>
+                </li>
+                <li className="navLi">
+                  <h3>
+                    <NavLink
+                      to="/users"
+                      activeStyle={{
+                        "backgroundColor":" #555",
+                        "color": "white",
+                        "textDecoration": "none"
+                      }}
+                    >
+                      Users
+                    </NavLink>
+                  </h3>
+                </li>
+              </ul>
+            </nav>
+
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/add-data">
+                <FormsNavbar />
+              </Route>
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route path="/createtimetable">
+                <TimeTableForm />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
       </div>
-
-      <Router>
-        <div style={{ margin: 0 }}>
-          <nav>
-            <ul className="navUl">
-              <li className="navLi">
-                <h3>
-                  <Link to="/" style={{ textDecoration: "none" }}>
-                    Home
-                  </Link>
-                </h3>
-              </li>
-              <li className="navLi">
-                <h3>
-                  <Link to="/add-data" style={{ textDecoration: "none" }}>
-                    Add data
-                  </Link>
-                </h3>
-              </li>
-              <li className="navLi">
-                <h3>
-                  <Link
-                    to="/createtimetable"
-                    style={{ textDecoration: "none" }}
-                  >
-                    Create Time Table
-                  </Link>
-                </h3>
-              </li>
-              <li className="navLi">
-                <h3>
-                  <Link to="/users" style={{ textDecoration: "none" }}>
-                    Users
-                  </Link>
-                </h3>
-              </li>
-            </ul>
-          </nav>
-
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/add-data">
-              <FormsNavbar />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/createtimetable">
-              <TimeTableForm />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
-  );
+    );
+  }
 }
 
+App.contextTypes = {
+  router: PropTypes.object
+};
+
 function Home() {
-  return (
-    <div style={{ marginLeft: "300px" }}>
-      Home
-    </div>
-  );
+  return <div style={{ marginLeft: "300px" }}>Home</div>;
 }
 
 function Users() {
   return <div style={{ marginLeft: "25%" }}>Users</div>;
 }
+
